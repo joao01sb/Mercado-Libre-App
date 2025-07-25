129e348e62b065d1b80edd12e405c89f43669a33
joao01sb <bonfimnew@gmail.com>
2025-07-24 22:44:01 -0300

    feat: Set up Hilt for dependency injection and restructure MainActivity

--- app/src/main/java/com/joao01sb/mercadolibreapp/MainActivity.kt
+++ app/src/main/java/com/joao01sb/mercadolibreapp/MainActivity.kt
@@ -3,45 +3,24 @@ package com.joao01sb.mercadolibreapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.joao01sb.mercadolibreapp.ui.theme.MercadoLibreAppTheme
+import com.joao01sb.mercadolibreapp.presentation.navigation.AppNavigation
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MercadoLibreAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                AppNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MercadoLibreAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/joao01sb/mercadolibreapp/MercadoLibreApp.kt
@@ -0,0 +1,7 @@
+package com.joao01sb.mercadolibreapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MercadoLibreApp : Application()
